781bf602b51c294cf1f01eeed65cf9f5989a3427
M. Alexander Morales <dvelopment.guru@gmail.com>
2019-03-13 19:11:40 -0400

    Update Company.php

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,12 +4,16 @@ class Company
 {
 	public function getAllCompanies()
 	{
-		$query = Database::getInstance()->getConnection()->query("
+		$result = Database::getInstance()->getConnection()->query("
 			SELECT companies.id, companies.name AS company
 			FROM companies
 			ORDER BY companies.name ASC
 		");
 		
-		return $query->fetch_all(MYSQLI_ASSOC);
+		$rows = [];
+		while ($row = $result->fetch_assoc()) {
+			$rows[] = $row;
+		}
+		return $rows;
 	}
 }
